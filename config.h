378fe8e44537280dff95ff1fd5e0f1c0315052f6
David Draguta <daviddraguta@gmail.com>
2020-02-21 02:35:11 -0500

    Patch line heights in.

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka:size=10:antialias=true:hint=true:hintstyle=hintfull; weight=medium: slant=roman: dpi=96"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
